211911b722b1b72ad5c1d2959a7ef9120b230970
Dan Stroot <dan.stroot@gmail.com>
2019-01-15 14:44:19 -0800

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# S3 bucket names must be unique
+REGION=us-west-2
+PROJECT=aws-s3-hosting
+BUCKET=$PROJECT-90278
+
+# Create bucket using cloudformation
+aws cloudformation deploy                     \
+    --template-file s3hosting.yaml    \
+    --stack-name $PROJECT                     \
+    --capabilities CAPABILITY_IAM             \
+    --parameter-overrides Name=$BUCKET
+
+# setup package.json to deploy
+echo -e "All done! Now add these lines to your package.json"
+echo -e 'file in the "scripts" section:\n'
+
+echo -e '"predeploy": "yarn build",'
+echo -e '"deploy": "aws s3 sync build/ s3://'$BUCKET'"'
+
+echo -e "\nThe URL is:"
+echo -e "http://"$BUCKET".s3-website-"$REGION".amazonaws.com\n"
---
+++ readme.md
@@ -0,0 +1,8 @@
+# S3 and Cloudfront hosting
+
+Hosting a Single Page App (SPA), built in a JavaScript framework such as Angular or React is simple. For these apps, you don’t need a server — all you need is somewhere that will serve a set of static files, and all the website logic is done client-side by JavaScript.
+
+### References
+
+- [Single-Page Apps on AWS, Part 1](https://medium.com/@P_Lessing/single-page-apps-on-aws-part-1-hosting-a-website-on-s3-3c9871f126)
+- [Single-Page Apps on AWS, Part 2](https://medium.com/@P_Lessing/single-page-apps-on-aws-part-2-deploying-a-compiled-frontend-33723e8f6814)
